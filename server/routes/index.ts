1366a35893a00e73cea598610eae63af14e3ca83
Sergey Sukiasyan <sergeysukiasyan@gmail.com>
2022-06-27 01:34:31 +0400

    Commit Files

--- server/routes/index.ts
+++ server/routes/index.ts
@@ -1,8 +1,6 @@
 import ErrorMiddleware from "../app/Middleware/ErrorMiddleware";
 import { app } from "../config/app";
 import { movies } from "./movies";
-import {auth} from "./auth";
 
-app.use('/auth', auth);
 app.use(movies);
 app.use(ErrorMiddleware);
