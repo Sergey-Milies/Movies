ddb50ed5a2c81f667c21fc1a620be9245d4d4d14
Sergey Sukiasyan <sergeysukiasyan@gmail.com>
2022-06-27 17:00:45 +0400

    Commit Files

--- client/src/Layout/Components/UI/Loader/Loader.tsx
+++ client/src/Layout/Components/UI/Loader/Loader.tsx
@@ -1,4 +1,4 @@
-import {FC} from "react";
+import {FC, useEffect, useState} from "react";
 import loader from '../../../../Static/images/loader.gif';
 import classes from './Loader.module.scss';
 import {Box} from "@mui/material";
@@ -6,9 +6,20 @@ import {isLoading} from "../../../../Store/moviesSlice/moviesSlice";
 import {useAppSelector} from "../../../../Store/hooks";
 
 const Loader: FC = () => {
+    const [show, setShow] = useState(true);
     const isLoad = useAppSelector(isLoading);
 
-    return isLoad ? (
+    useEffect(() => {
+        if(!isLoad) {
+            setTimeout(() => {
+                setShow(false);
+            }, 500);
+        } else {
+            setShow(true);
+        }
+    }, [isLoad]);
+
+    return show ? (
         <Box component='div' className={classes.Loader}>
             <img src={loader} />
         </Box>
